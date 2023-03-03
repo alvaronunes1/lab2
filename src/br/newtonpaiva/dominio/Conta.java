42f4da17e3aff8195d79cd54247c7ec7039f4131
12116455 <alvaro.nunes15@gmail.com>
2023-03-03 20:25:24 -0300

    feat: cria a classe conta

--- src/br/newtonpaiva/dominio/Conta.java
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -1,17 +1,32 @@
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class Conta {
 
     private Integer numero;
 
     private Double saldo;
 
+    public Conta(){
+        this (null);
+    }
+
+    public Conta(Integer numero) {
+        this(numero,0.0);
+    }
+
+    public Conta (Integer numero, Double saldo){
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
     public Double sacar(Double valor) {
         return 0.0;
     }
 
     public Double depositar(Double valor) {
-        if(valor == null || valor <= 0 )
+        if (valor == null || valor <= 0)
             throw new IllegalArgumentException("Valor menor ou igual a 0");
 
         saldo += valor;
@@ -32,5 +47,18 @@ public class Conta {
 
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
     }
 }
